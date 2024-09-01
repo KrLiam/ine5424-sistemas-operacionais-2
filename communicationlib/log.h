44239517baa5d5244e8eb786d532098ead319de3
Fabricio Duarte <fabricio.duarte.jr@gmail.com>
2024-09-01 15:18:08 -0300

    colocar log() dentro do logger

--- communicationlib/log.h
+++ communicationlib/log.h
@@ -5,14 +5,17 @@
 #include <iostream>
 #include <utility>
 
-#define log_error(...) log("ERROR", __FILE__, __LINE__, ##__VA_ARGS__)
-#define log_warn(...) log("WARN", __FILE__, __LINE__, ##__VA_ARGS__)
-#define log_info(...) log("INFO", __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_error(...) Logger::log("ERROR", __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_warn(...) Logger::log("WARN", __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_info(...) Logger::log("INFO", __FILE__, __LINE__, ##__VA_ARGS__)
 
-template<typename ...Args>
-void log(const char* level, const char* file, int line, Args && ...args)
-{
-    auto t = std::time(nullptr);
-    auto tm = *std::localtime(&t);
-    (std::cout << std::put_time(&tm, "%H:%M:%S ") << std::format("{} [{}:{}]: ", level, file, line) << ... << args) << std::endl;
-}
+class Logger {
+public:
+    template<typename ...Args>
+    static void log(const char* level, const char* file, int line, Args && ...args)
+    {
+        auto t = std::time(nullptr);
+        auto tm = *std::localtime(&t);
+        (std::cout << std::put_time(&tm, "%H:%M:%S ") << std::format("{} [{}:{}]: ", level, file, line) << ... << args) << std::endl;
+    }
+};
