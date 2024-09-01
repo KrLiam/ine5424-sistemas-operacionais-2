c74f391851bcf765fc1bbc2578df6337457cbb34
Fabricio Duarte <fabricio.duarte.jr@gmail.com>
2024-09-01 14:55:14 -0300

    corrigir horario nos logs

--- communicationlib/log.h
+++ communicationlib/log.h
@@ -1,15 +1,18 @@
 #pragma once
 
 #include <format>
+#include <iomanip>
 #include <iostream>
 #include <utility>
 
-#define log_error(...) log("ERROR", __TIME__, __FILE__, __LINE__, ##__VA_ARGS__)
-#define log_warn(...) log("WARN", __TIME__, __FILE__, __LINE__, ##__VA_ARGS__)
-#define log_info(...) log("INFO", __TIME__, __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_error(...) log("ERROR", __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_warn(...) log("WARN", __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_info(...) log("INFO", __FILE__, __LINE__, ##__VA_ARGS__)
 
 template<typename ...Args>
-void log(const char* level, const char* time, const char* file, int line, Args && ...args)
+void log(const char* level, const char* file, int line, Args && ...args)
 {
-    (std::cout << std::format("{} {} [{}:{}]: ", time, level, file, line) << ... << args) << std::endl;
+    auto t = std::time(nullptr);
+    auto tm = *std::localtime(&t);
+    (std::cout << std::put_time(&tm, "%H:%M:%S ") << std::format("{} [{}:{}]: ", level, file, line) << ... << args) << std::endl;
 }
