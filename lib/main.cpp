6e488993722da0ec78e5f0cf054bc0bbb6dc4e08
tiago <dexe.me12@gmail.com>
2024-09-03 20:32:23 -0300

    Refatoração no gerenciamento de threads e argumentos em main.cpp
    Troquei o uso do pthread pelo std::thread do C++11, que é mais seguro e alinhado com os padrões modernos. Também simplifiquei as estruturas de dados, dando nomes mais claros, e transformei o tamanho do buffer em uma constante.

--- lib/main.cpp
+++ lib/main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <thread>
-
 #include "core/log.h"
 #include "communication/node.h"
 #include "core/format.h"
 #include "communication/reliable_communication.h"
 
-std::size_t BUFFER_SIZE = 1024;
+const std::size_t BUFFER_SIZE = 1024;
 
 struct Arguments {
     std::string node_id;
@@ -19,67 +18,54 @@ Arguments parse_arguments(int argc, char* argv[]) {
             format("Missing node id argument. Usage:\n%s <id-int>", argv[0])
         );
     }
-
     ConfigReader reader(argv[1]);
     std::string node_id = reader.read_word();
-
     if (!reader.eof()) {
         throw std::invalid_argument(
             format("Node id must be alphanumeric.", argv[1])
         );
     }
-
     return Arguments{node_id};
 }
 
 struct ThreadArgs {
-    ReliableCommunication* comm;
+    ReliableCommunication* communication;
 };
 
-void* server(void *args) {
-    ThreadArgs* targs = (ThreadArgs*) args;
-    ReliableCommunication* comm = targs->comm;
-
+void server(ThreadArgs* args) {
+    ReliableCommunication* comm = args->communication;
     char buffer[BUFFER_SIZE];
     std::size_t len = -1;
     while (len != 0) {
         len = comm->receive(buffer);
         log_info("Received '", std::string(buffer).c_str(), "' from <IP>.");
     }
-    return NULL;
 }
 
-void* client(void *args) {
-    ThreadArgs* targs = (ThreadArgs*) args;
-    ReliableCommunication* comm = targs->comm;
-
+void client(ThreadArgs* args) {
+    ReliableCommunication* comm = args->communication;
     char msg[BUFFER_SIZE] = "Hello";
     while (true) {
         std::string id;
         std::cin >> id;
-
         if (id == "exit") break;
-
         comm->send(id, msg);
     }
-    
     log_info("Exited client.");
-    return NULL;
 }
 
-void run_process(std::string node_id)
-{
+void run_process(const std::string& node_id) {
     ReliableCommunication comm(node_id, BUFFER_SIZE);
-
     Node local_node = comm.get_node(node_id);
-    log_info("Local node endpoint is ", local_node.get_address().to_string() , ".");
+    log_info("Local node endpoint is ", local_node.get_address().to_string(), ".");
 
     ThreadArgs targs = { &comm };
-    pthread_t server_thread;
-    pthread_t client_thread;
-    pthread_create(&server_thread, NULL, server, &targs);
-    pthread_create(&client_thread, NULL, client, &targs);
-    pthread_join(client_thread, NULL);
+
+    std::thread server_thread(server, &targs);
+    std::thread client_thread(client, &targs);
+
+    client_thread.join();
+    server_thread.detach();
 }
 
 int main(int argc, char* argv[]) {
@@ -87,7 +73,7 @@ int main(int argc, char* argv[]) {
         Arguments args = parse_arguments(argc, argv);
         run_process(args.node_id);
     }
-    catch (std::exception& error) {
+    catch (const std::exception& error) {
         log_error(error.what());
     }
 }
