bc8dfba0320d611a553490f083909d316c31cc5a
Fabricio Duarte <fabricio.duarte.jr@gmail.com>
2024-09-12 22:54:53 -0300

    Corrigir SIGABORT devido a condição de corrida ao acessar lista de pacotes a serem enviados

--- lib/communication/transmission_queue.cpp
+++ lib/communication/transmission_queue.cpp
@@ -6,21 +6,26 @@ TransmissionQueue::TransmissionQueue() : fragments_awaiting_ack(), fragments_to_
 
 bool TransmissionQueue::has_sent_everything()
 {
-    return fragments_to_send.size() == 0;
+    queue_mutex.lock();
+    bool result = fragments_to_send.size() == 0;
+    queue_mutex.unlock();
+    return result;
 }
 
 void TransmissionQueue::add_packet_to_queue(Packet packet)
 {
+    queue_mutex.lock();
     fragments_to_send.push_back(packet);
+    queue_mutex.unlock();
 }
 
 void TransmissionQueue::mark_packet_as_acked(Packet packet)
 {
-    // controlar com mtx
     uint32_t fragment_number = (uint32_t)packet.data.header.fragment_num;
     fragments_awaiting_ack.erase(std::remove(fragments_awaiting_ack.begin(), fragments_awaiting_ack.end(), fragment_number), fragments_awaiting_ack.end());
     // fragments_to_send.erase(std::remove(fragments_to_send.begin(), fragments_to_send.end(), packet), fragments_to_send.end());
     // TODO: achar um jeito melhor de remover o fragmento da lista de fragmentos pra enviar
+    queue_mutex.lock();
     for (std::size_t i = 0; i < fragments_to_send.size(); i++)
     {
         Packet p = fragments_to_send[i];
@@ -30,14 +35,15 @@ void TransmissionQueue::mark_packet_as_acked(Packet packet)
             break;
         }
     }
+    queue_mutex.unlock();
 }
 
 void TransmissionQueue::send_timedout_packets(Channel *channel)
 {
-    // TODO: mutex pro vetor pra não ter condição de corrida entre thread de recebimento e envio
     uint64_t now = DateUtils::now();
     std::vector<std::size_t> indexes_to_remove = std::vector<std::size_t>();
 
+    queue_mutex.lock();
     for (std::size_t i = 0; i < fragments_to_send.size(); i++)
     {
         Packet packet = fragments_to_send[i];
@@ -63,4 +69,5 @@ void TransmissionQueue::send_timedout_packets(Channel *channel)
         fragments_to_send.erase(fragments_to_send.begin() + idx + offset);
         offset--;
     }
+    queue_mutex.unlock();
 }
--- lib/communication/transmission_queue.h
+++ lib/communication/transmission_queue.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <mutex>
 #include <vector>
 
 #include "channels/channel.h"
@@ -12,11 +13,22 @@ private:
     std::vector<uint32_t> fragments_awaiting_ack;
     std::vector<Packet> fragments_to_send;
 
+    std::mutex queue_mutex;
+
 public:
     TransmissionQueue();
 
     bool has_sent_everything();
+    /**
+     * Used by the received thread to insert a packet into the send queue.
+    */
     void add_packet_to_queue(Packet packet);
+    /**
+     * Used by the sender thread to remove a packet from the pending ACK list and the send queue.
+    */
     void mark_packet_as_acked(Packet packet);
+    /**
+     * Used by the sender thread to send packets from the send queue.
+    */
     void send_timedout_packets(Channel *channel);
 };
