3bf264b472a93633bedcbd45b0b57bf4e784597b
tiago <dexe.me12@gmail.com>
2024-09-18 10:03:08 -0300

    Adiciona override nas funções virtuais

--- lib/pipeline/fragmentation/fragmentation_layer.h
+++ lib/pipeline/fragmentation/fragmentation_layer.h
@@ -1,26 +1,24 @@
 #pragma once
 
-#include <cmath>
 #include <map>
 
 #include "pipeline/fragmentation/fragment_assembler.h"
 #include "pipeline/pipeline_step.h"
 
-class FragmentationLayer : public PipelineStep
+class FragmentationLayer final : public PipelineStep
 {
-private:
     std::map<std::string, FragmentAssembler> assembler_map;
 
 public:
     FragmentationLayer(PipelineHandler handler, GroupRegistry *gr);
     ~FragmentationLayer() override;
 
-    void service();
+    void service() override;
 
-    void send(Message);
-    void send(Packet);
+    void send(Message) override;
+    void send(Packet) override;
 
-    void receive(Packet);
+    void receive(Packet) override;
 
     bool is_message_complete(std::string node_id)
     {
@@ -29,10 +27,10 @@ public:
         FragmentAssembler &assembler = assembler_map.at(node_id);
         return assembler.has_received_all_packets();
     }
-    Message assemble_message(std::string node_id)
+    Message assemble_message(const std::string& node_id)
     {
         FragmentAssembler &assembler = assembler_map.at(node_id);
-        Message message = assembler.assemble();
+        Message const message = assembler.assemble();
         assembler_map.erase(node_id);
         return message;
     }
