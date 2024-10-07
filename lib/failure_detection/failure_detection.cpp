258c4c33d2af144854956de6cb1ba139a3538b49
tiago <dexe.me12@gmail.com>
2024-10-07 18:40:27 -0300

    Cria cpp pro failure_detection

---
+++ lib/failure_detection/failure_detection.cpp
@@ -0,0 +1,74 @@
+#include "failure_detection.h"
+#include "utils/date.h"
+
+FailureDetection::FailureDetection(std::shared_ptr<GroupRegistry> gr, EventBus &event_bus, unsigned int alive)
+    : gr(gr), event_bus(event_bus), alive(alive), keep_alive(alive * MAX_HEARTBEAT_TRIES), running(true)
+{
+    attach();
+    failure_detection_thread = std::thread([this]() { failure_detection_routine(); });
+}
+
+FailureDetection::~FailureDetection()
+{
+    running = false;
+    if (failure_detection_thread.joinable())
+        failure_detection_thread.join();
+}
+
+void FailureDetection::connection_established(const ConnectionEstablished &event)
+{
+    last_alive.emplace(event.node.get_id(), DateUtils::now());
+}
+
+void FailureDetection::connection_closed(const ConnectionClosed &event)
+{
+    last_alive.erase(event.node.get_id());
+}
+
+void FailureDetection::heartbeat_received(const HeartbeatReceived &event)
+{
+    log_trace("Received heartbeat from ", event.remote_node.get_address().to_string(), ".");
+    last_alive[event.remote_node.get_id()] = DateUtils::now();
+}
+
+void FailureDetection::failure_detection_routine()
+{
+    log_info("Initialized failure detection thread.");
+    while (running)
+    {
+        uint64_t now = DateUtils::now();
+
+        for (auto &[id, node] : gr->get_nodes())
+        {
+            Connection &conn = gr->get_connection(node);
+            if (conn.get_state() != ConnectionState::ESTABLISHED)
+                continue;
+
+            if (now - last_alive[node.get_id()] > keep_alive)
+            {
+                log_warn("No heartbeat from ",
+                         node.get_address().to_string(),
+                         " in the last ",
+                         keep_alive,
+                         " milliseconds; marking node as dead.");
+                event_bus.notify(NodeDeath(node));
+                continue;
+            }
+
+            conn.heartbeat();
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(alive));
+    }
+    log_info("Closed failure detection thread.");
+}
+
+void FailureDetection::attach()
+{
+    obs_connection_established.on(std::bind(&FailureDetection::connection_established, this, _1));
+    obs_connection_closed.on(std::bind(&FailureDetection::connection_closed, this, _1));
+    obs_heartbeat_received.on(std::bind(&FailureDetection::heartbeat_received, this, _1));
+    event_bus.attach(obs_connection_established);
+    event_bus.attach(obs_connection_closed);
+    event_bus.attach(obs_heartbeat_received);
+}
--- lib/failure_detection/failure_detection.h
+++ lib/failure_detection/failure_detection.h
@@ -4,7 +4,7 @@
 #include <functional>
 #include <thread>
 #include <map>
-
+#include <memory>
 #include "communication/group_registry.h"
 #include "core/event.h"
 #include "core/event_bus.h"
@@ -13,7 +13,7 @@
 
 class FailureDetection
 {
-    bool running = true;
+    bool running;
 
     unsigned int alive;
     unsigned int keep_alive;
@@ -30,77 +30,18 @@ class FailureDetection
     Observer<ConnectionClosed> obs_connection_closed;
     Observer<HeartbeatReceived> obs_heartbeat_received;
 
-    void connection_established(const ConnectionEstablished &event)
-    {
-        last_alive.emplace(event.node.get_id(), DateUtils::now());
-    }
+    void connection_established(const ConnectionEstablished &event);
 
-    void connection_closed(const ConnectionClosed &event)
-    {
-        last_alive.erase(event.node.get_id());
-    }
+    void connection_closed(const ConnectionClosed &event);
 
-    void heartbeat_received(const HeartbeatReceived &event)
-    {
-        log_trace("Received heartbeat from ", event.remote_node.get_address().to_string(), ".");
-        last_alive[event.remote_node.get_id()] = DateUtils::now();
-    }
+    void heartbeat_received(const HeartbeatReceived &event);
 
-    void failure_detection_routine()
-    {
-        log_info("Initialized failure detection thread.");
-        while (running)
-        {
-            uint64_t now = DateUtils::now();
-
-            for (auto &[id, node] : gr->get_nodes())
-            {
-                Connection &conn = gr->get_connection(node);
-                if (conn.get_state() != ConnectionState::ESTABLISHED)
-                    continue;
-
-                if (now - last_alive[node.get_id()] > keep_alive)
-                {
-                    log_warn("No heartbeat from ",
-                             node.get_address().to_string(),
-                             " in the last ",
-                             keep_alive,
-                             " milliseconds; marking node as dead.");
-                    event_bus.notify(NodeDeath(node));
-                    continue;
-                }
-
-                conn.heartbeat();
-            }
-
-            std::this_thread::sleep_for(std::chrono::milliseconds(alive));
-        }
-        log_info("Closed failure detection thread.");
-    }
+    void failure_detection_routine();
 
 public:
-    FailureDetection(std::shared_ptr<GroupRegistry> gr, EventBus &event_bus, unsigned int alive) : gr(gr), event_bus(event_bus), alive(alive), keep_alive(alive * MAX_HEARTBEAT_TRIES)
-    {
-        attach();
+    FailureDetection(std::shared_ptr<GroupRegistry> gr, EventBus &event_bus, unsigned int alive);
 
-        failure_detection_thread = std::thread([this]()
-                                               { failure_detection_routine(); });
-    }
+    ~FailureDetection();
 
-    ~FailureDetection()
-    {
-        running = false;
-        if (failure_detection_thread.joinable())
-            failure_detection_thread.join();
-    }
-
-    void attach()
-    {
-        obs_connection_established.on(std::bind(&FailureDetection::connection_established, this, _1));
-        obs_connection_closed.on(std::bind(&FailureDetection::connection_closed, this, _1));
-        obs_heartbeat_received.on(std::bind(&FailureDetection::heartbeat_received, this, _1));
-        event_bus.attach(obs_connection_established);
-        event_bus.attach(obs_connection_closed);
-        event_bus.attach(obs_heartbeat_received);
-    }
+    void attach();
 };
