7fcb60b400104fd584e4db56128e9b1f0500f58e
anaard <anareginadelazzari@gmail.com>
2024-10-21 21:03:54 -0300

    separa raft em .h e .cpp

---
+++ lib/raft/raft.cpp
@@ -0,0 +1,353 @@
+#include "raft/raft.h"
+
+RaftManager::RaftManager(
+    std::map<std::string, Connection> &connections,
+    NodeMap &nodes,
+    Node &local_node,
+    Pipeline &pipeline) : data_filename(format("%s.raft", local_node.get_id().c_str())),
+                          state(FOLLOWER),
+                          connections(connections),
+                          nodes(nodes),
+                          local_node(local_node),
+                          leader(nullptr),
+                          pipeline(pipeline),
+                          timer_id(0),
+                          election_time_dis(3000, 4000)
+{
+    // TODO: o election_timeout_dis tem que ser definido com base no `alive`
+
+    read_data();
+
+    obs_packet_received.on(std::bind(&RaftManager::packet_received, this, _1));
+    pipeline.attach(obs_packet_received);
+
+    set_election_timer();
+}
+
+void RaftManager::read_data()
+{
+    std::ifstream f(data_filename);
+
+    if (!f.good())
+    {
+        memset(&data, 0, sizeof(RaftPersistentData));
+        return;
+    }
+
+    f.read((char *)&data, sizeof(RaftPersistentData));
+    f.close();
+}
+
+void RaftManager::save_data()
+{
+    std::ofstream f(data_filename);
+    f.write((char *)&data, sizeof(RaftPersistentData));
+    f.close();
+}
+
+void RaftManager::change_state(RaftState new_state)
+{
+    if (state == new_state)
+        return;
+
+    state = new_state;
+
+    cancel_election_timer();
+
+    if (post_transition_handlers.contains(state))
+        post_transition_handlers.at(state)();
+}
+
+void RaftManager::send_vote(Packet packet)
+{
+    log_info(
+        "Voting for ",
+        packet.meta.origin.to_string(),
+        ".");
+
+    RaftRPCData reply_data =
+        {
+            success : true
+        };
+
+    PacketData data = packet.data;
+    data.header.flags = data.header.flags | ACK;
+    data.header.id.origin = local_node.get_address();
+    memcpy(data.message_data, &reply_data, sizeof(reply_data));
+
+    PacketMetadata meta = {
+        transmission_uuid : UUID(""),
+        origin : local_node.get_address(),
+        destination : packet.meta.origin,
+        message_length : sizeof(reply_data),
+        expects_ack : 0,
+        silent : 0
+    };
+
+    Packet p = {
+        data : data,
+        meta : meta
+    };
+
+    Node &destination = nodes.get_node(p.meta.destination);
+    Connection &connection = connections.at(destination.get_id());
+    connection.transmit(p);
+}
+
+void RaftManager::send_request_vote()
+{
+    RaftRPCData rvo_data =
+        {
+            success : 0
+        };
+
+    PacketData data;
+    memset(&data, 0, sizeof(PacketData));
+    data.header = {
+        id : {
+            origin : local_node.get_address(),
+            msg_num : 0
+        },
+        fragment_num : 0,
+        checksum : 0,
+        flags : RVO,
+        type : MessageType::RAFT
+    };
+    memcpy(data.message_data, &rvo_data, sizeof(rvo_data));
+
+    PacketMetadata meta = {
+        transmission_uuid : UUID(""),
+        origin : local_node.get_address(),
+        destination : {BROADCAST_ADDRESS, 0},
+        message_length : sizeof(RaftRPCData),
+        expects_ack : 0,
+        silent : 0
+    };
+
+    Packet p = {
+        data : data,
+        meta : meta
+    };
+
+    // broadcast_connection.transmit(p, BroadcastType::BEB);
+    // TODO: usar o broadcast connection aqui pra fazer um beb
+    for (auto &[id, conn] : connections)
+    {
+        p.meta.destination = nodes.get_node(id).get_address();
+        conn.transmit(p);
+    }
+}
+
+void RaftManager::packet_received(const PacketReceived &event)
+{
+    Packet &packet = event.packet;
+    MessageType type = packet.data.header.get_message_type();
+
+    if (type != MessageType::RAFT && type != MessageType::HEARTBEAT)
+        return;
+
+    if (!packet.silent())
+    {
+        log_trace(
+            "Packet ",
+            packet.to_string(PacketFormat::RECEIVED),
+            " received by raft manager.");
+    }
+
+    // if (packet.data.header.is_ack()) pipeline.notify(PacketAckReceived(packet));
+
+    packet_receive_handlers.at(state)(packet);
+}
+
+void RaftManager::on_follower()
+{
+    data.voted_for = nullptr;
+    save_data();
+
+    set_election_timer();
+}
+
+void RaftManager::follower_timeout()
+{
+    change_state(CANDIDATE);
+}
+
+void RaftManager::follower_receive(Packet packet)
+{
+    if (packet.data.header.is_ldr())
+    {
+        set_election_timer();
+        if (leader == nullptr || leader->get_address() != packet.meta.origin)
+            follow(nodes.get_node(packet.meta.origin));
+        return;
+    }
+
+    if (packet.data.header.is_rvo())
+    {
+        if (packet.data.header.is_ack())
+        {
+            log_debug("Received a vote, but we are not a candidate; ignoring.");
+            return;
+        }
+
+        if (leader != nullptr)
+        {
+            log_debug("Received a vote request, but we already have a leader; ignoring.");
+            return;
+        }
+
+        if (data.voted_for != nullptr)
+        {
+            log_debug("Received vote request, but we already voted for someone.");
+            return;
+        }
+
+        if (should_grant_vote(packet.meta.origin))
+        {
+            data.voted_for = &nodes.get_node(packet.meta.origin);
+            save_data();
+            send_vote(packet);
+            set_election_timer();
+        }
+    }
+}
+
+void RaftManager::on_candidate()
+{
+    candidate_timeout();
+}
+
+void RaftManager::candidate_timeout()
+{
+    log_info("Candidate timed out; starting new election.");
+    data.voted_for = nullptr;
+    save_data();
+
+    leader = nullptr;
+
+    received_votes.clear();
+
+    set_election_timer();
+    send_request_vote();
+    check_if_won_election();
+}
+
+void RaftManager::candidate_receive(Packet packet)
+{
+    if (packet.data.header.is_ldr())
+    {
+        follow(nodes.get_node(packet.meta.origin));
+        return;
+    }
+
+    if (packet.data.header.is_rvo())
+    {
+        if (packet.data.header.is_ack())
+        {
+            RaftRPCData *rvo_data = reinterpret_cast<RaftRPCData *>(packet.data.message_data);
+            if (rvo_data->success)
+            {
+                log_debug(
+                    "Received vote from ",
+                    packet.meta.origin.to_string(),
+                    ".");
+                received_votes.emplace(packet.meta.origin);
+                check_if_won_election();
+            }
+            return;
+        }
+
+        if (data.voted_for != nullptr)
+        {
+            log_debug("Received vote request, but we already voted for someone.");
+            return;
+        }
+
+        if (should_grant_vote(packet.meta.origin))
+        {
+            data.voted_for = &nodes.get_node(packet.meta.origin);
+            save_data();
+            send_vote(packet);
+            set_election_timer();
+        }
+    }
+}
+
+void RaftManager::on_leader()
+{
+    leader = &local_node;
+    leader->set_leader(true);
+}
+
+void RaftManager::leader_receive(Packet packet)
+{
+    if (packet.data.header.is_ldr())
+    {
+        if (packet.meta.origin != local_node.get_address())
+            follow(nodes.get_node(packet.meta.origin));
+    }
+}
+
+void RaftManager::set_election_timer()
+{
+    cancel_election_timer();
+    int election_time = election_time_dis(rc_random::gen);
+    timer_id = timer.add(election_time, election_timeout_handlers.at(state));
+}
+
+void RaftManager::cancel_election_timer()
+{
+    if (timer_id != 0)
+        timer.cancel(timer_id);
+}
+
+void RaftManager::follow(Node &node)
+{
+    log_info(
+        "New leader is ",
+        node.get_address().to_string(),
+        ".");
+    if (leader != nullptr)
+        leader->set_leader(false);
+    leader = &node;
+    leader->set_leader(true);
+    cancel_election_timer();
+    change_state(FOLLOWER);
+}
+
+bool RaftManager::should_grant_vote(SocketAddress address)
+{
+    for (auto &[id, node] : nodes)
+    {
+        if (!node.is_alive())
+            continue;
+        if (strcmp(node.get_address().to_string().c_str(), address.to_string().c_str()) < 0)
+        {
+            log_debug("Not granting vote to ", address.to_string(), "; ", node.get_address().to_string(), " has a higher priority.");
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void RaftManager::check_if_won_election()
+{
+    unsigned int quota = get_quota();
+    if (received_votes.size() >= quota)
+    {
+        log_info("Received quota, we are now the leader.");
+        cancel_election_timer();
+        change_state(LEADER);
+    }
+}
+
+unsigned int RaftManager::get_quota()
+{
+    unsigned int alive = 0;
+    for (auto &[_, node] : nodes)
+        if (node.is_alive())
+            alive++;
+
+    return (alive / 2) + 1;
+}
--- lib/raft/raft.h
+++ lib/raft/raft.h
@@ -22,7 +22,7 @@ enum RaftState
 
 struct RaftPersistentData
 {
-    Node* voted_for;
+    Node *voted_for;
 };
 
 class RaftManager
@@ -33,20 +33,22 @@ class RaftManager
 
     RaftState state;
 
-    std::map<std::string, Connection>& connections;
+    std::map<std::string, Connection> &connections;
     // BroadcastConnection& broadcast_connection;
 
-    NodeMap& nodes;
-    Node& local_node;
-    Node* leader;
+    NodeMap &nodes;
+    Node &local_node;
+    Node *leader;
 
-    Pipeline& pipeline;
+    Pipeline &pipeline;
 
     std::unordered_set<SocketAddress> received_votes;
 
     int timer_id;
     Timer timer;
     std::uniform_int_distribution<> election_time_dis;
+
+    Observer<PacketReceived> obs_packet_received;
 
     const std::map<RaftState, std::function<void()>> post_transition_handlers = {
         {FOLLOWER, std::bind(&RaftManager::on_follower, this)},
@@ -56,344 +58,42 @@ class RaftManager
     const std::map<RaftState, std::function<void()>> election_timeout_handlers = {
         {FOLLOWER, std::bind(&RaftManager::follower_timeout, this)},
         {CANDIDATE, std::bind(&RaftManager::candidate_timeout, this)}};
-    
+
     const std::map<RaftState, std::function<void(Packet)>> packet_receive_handlers = {
         {FOLLOWER, std::bind(&RaftManager::follower_receive, this, _1)},
         {CANDIDATE, std::bind(&RaftManager::candidate_receive, this, _1)},
         {LEADER, std::bind(&RaftManager::leader_receive, this, _1)}};
 
-    void read_data()
-    {
-        std::ifstream f(data_filename);
+    void read_data();
+    void save_data();
 
-        if (!f.good())
-        {
-            memset(&data, 0, sizeof(RaftPersistentData));
-            return;
-        }
+    void change_state(RaftState new_state);
 
-        f.read((char*)&data, sizeof(RaftPersistentData));
-        f.close();
-    }
+    void send_vote(Packet packet);
+    void send_request_vote();
 
-    void save_data()
-    {
-        std::ofstream f(data_filename);
-        f.write((char*)&data, sizeof(RaftPersistentData));
-        f.close();
-    }
+    void packet_received(const PacketReceived &event);
 
-    void change_state(RaftState new_state)
-    {
-        if (state == new_state) return;
+    void on_follower();
+    void follower_timeout();
+    void follower_receive(Packet packet);
 
-        state = new_state;
+    void on_candidate();
+    void candidate_timeout();
+    void candidate_receive(Packet packet);
 
-        cancel_election_timer();
+    void on_leader();
+    void leader_receive(Packet packet);
 
-        if (post_transition_handlers.contains(state)) post_transition_handlers.at(state)();
-    }
+    void set_election_timer();
+    void cancel_election_timer();
 
-    void follower_timeout()
-    {
-        change_state(CANDIDATE);
-    }
+    void follow(Node &node);
+    bool should_grant_vote(SocketAddress address);
 
-    void follower_receive(Packet packet)
-    {
-        if (packet.data.header.is_ldr())
-        {
-            set_election_timer();
-            if (leader == nullptr || leader->get_address() != packet.meta.origin) follow(nodes.get_node(packet.meta.origin));
-            return;
-        }
-        
-        if (packet.data.header.is_rvo())
-        {
-            if (packet.data.header.is_ack())
-            {
-                log_debug("Received a vote, but we are not a candidate; ignoring.");
-                return;
-            }
-
-            if (leader != nullptr)
-            {
-                log_debug("Received a vote request, but we already have a leader; ignoring.");
-                return;
-            }
-
-            if (data.voted_for != nullptr)
-            {
-                log_debug("Received vote request, but we already voted for someone.");
-                return;
-            }
-
-            if (should_grant_vote(packet.meta.origin))
-            {
-                data.voted_for = &nodes.get_node(packet.meta.origin);
-                save_data();
-                send_vote(packet);
-                set_election_timer();
-            }
-        }
-    }
-
-    void on_follower()
-    {
-        data.voted_for = nullptr;
-        save_data();
-
-        set_election_timer();
-    }
-
-    void send_vote(Packet packet)
-    {
-        log_info(
-            "Voting for ",
-            packet.meta.origin.to_string(),
-            ".");
-
-        RaftRPCData reply_data = 
-        {
-            success : true
-        };
-
-        PacketData data = packet.data;
-        data.header.flags = data.header.flags | ACK;
-        data.header.id.origin = local_node.get_address();
-        memcpy(data.message_data, &reply_data, sizeof(reply_data));
-
-        PacketMetadata meta = {
-            transmission_uuid : UUID(""),
-            origin : local_node.get_address(),
-            destination : packet.meta.origin,
-            message_length : sizeof(reply_data),
-            expects_ack : 0,
-            silent : 0
-        };
-
-        Packet p = {
-            data : data,
-            meta : meta
-        };
-
-        Node& destination = nodes.get_node(p.meta.destination);
-        Connection& connection = connections.at(destination.get_id());
-        connection.transmit(p);
-    }
-
-    void on_candidate()
-    {
-        candidate_timeout();
-    }
-
-    void candidate_timeout()
-    {
-        log_info("Candidate timed out; starting new election.");
-        data.voted_for = nullptr;
-        save_data();
-        
-        leader = nullptr;
-
-        received_votes.clear();
-
-        set_election_timer();
-        send_request_vote();
-        check_if_won_election();
-    }
-
-    void set_election_timer()
-    {
-        cancel_election_timer();
-        int election_time = election_time_dis(rc_random::gen);
-        timer_id = timer.add(election_time, election_timeout_handlers.at(state));
-    }
-
-    void cancel_election_timer()
-    {
-        if (timer_id != 0) timer.cancel(timer_id);
-    }
-
-    void send_request_vote()
-    { 
-        RaftRPCData rvo_data = 
-        {
-            success : 0
-        };
-
-        PacketData data;
-        memset(&data, 0, sizeof(PacketData));
-        data.header = {
-            id : {
-                origin : local_node.get_address(),
-                msg_num : 0
-            },
-            fragment_num: 0,
-            checksum: 0,
-            flags: RVO,
-            type : MessageType::RAFT
-        };
-        memcpy(data.message_data, &rvo_data, sizeof(rvo_data));
-
-        PacketMetadata meta = {
-            transmission_uuid : UUID(""),
-            origin : local_node.get_address(),
-            destination : {BROADCAST_ADDRESS, 0},
-            message_length : sizeof(RaftRPCData),
-            expects_ack : 0,
-            silent : 0
-        };
-
-        Packet p = {
-            data : data,
-            meta : meta
-        };
-
-        // broadcast_connection.transmit(p, BroadcastType::BEB);
-        // TODO: usar o broadcast connection aqui pra fazer um beb
-        for (auto& [id, conn] : connections)
-        {
-            p.meta.destination = nodes.get_node(id).get_address();
-            conn.transmit(p);
-        }
-    }
-
-    void check_if_won_election()
-    {
-        unsigned int quota = get_quota();
-        if (received_votes.size() >= quota)
-        {
-            log_info("Received quota, we are now the leader.");
-            cancel_election_timer();
-            change_state(LEADER);
-        }
-    }
-
-    unsigned int get_quota()
-    {
-        unsigned int alive = 0;
-        for (auto& [_, node] : nodes)
-            if (node.is_alive()) alive++;
-
-        return (alive / 2) + 1;
-    }
-
-    void candidate_receive(Packet packet)
-    {
-        if (packet.data.header.is_ldr())
-        {
-            follow(nodes.get_node(packet.meta.origin));
-            return;
-        }
-
-        if (packet.data.header.is_rvo())
-        {
-            if (packet.data.header.is_ack())
-            {
-                RaftRPCData* rvo_data = reinterpret_cast<RaftRPCData*>(packet.data.message_data);
-                if (rvo_data->success)
-                {
-                    log_debug(
-                        "Received vote from ",
-                        packet.meta.origin.to_string(),
-                        ".");
-                    received_votes.emplace(packet.meta.origin);
-                    check_if_won_election();
-                }
-                return;
-            }
-
-            if (data.voted_for != nullptr)
-            {
-                log_debug("Received vote request, but we already voted for someone.");
-                return;
-            }
-
-            if (should_grant_vote(packet.meta.origin))
-            {
-                data.voted_for = &nodes.get_node(packet.meta.origin);
-                save_data();
-                send_vote(packet);
-                set_election_timer();
-            }
-        }
-    }
-
-    void on_leader()
-    {
-        leader = &local_node;
-        leader->set_leader(true);
-    }
-
-    void leader_receive(Packet packet)
-    {
-        if (packet.data.header.is_ldr())
-        {
-            if (packet.meta.origin != local_node.get_address()) follow(nodes.get_node(packet.meta.origin));
-        }
-    }
-
-    void follow(Node& node)
-    {
-        log_info(
-            "New leader is ",
-            node.get_address().to_string(),
-            "."
-            );
-        if (leader != nullptr) leader->set_leader(false);
-        leader = &node;
-        leader->set_leader(true);
-        cancel_election_timer();
-        change_state(FOLLOWER);
-    }
-
-    Observer<PacketReceived> obs_packet_received;
-
-    void packet_received(const PacketReceived& event)
-    {
-        Packet& packet = event.packet;
-        MessageType type = packet.data.header.get_message_type();
-
-        if (type != MessageType::RAFT && type != MessageType::HEARTBEAT) return;
-
-        if (!packet.silent())
-        {
-            log_trace(
-            "Packet ",
-            packet.to_string(PacketFormat::RECEIVED),
-            " received by raft manager."
-            );
-        }
-
-        // if (packet.data.header.is_ack()) pipeline.notify(PacketAckReceived(packet));
-
-        packet_receive_handlers.at(state)(packet);
-    }
-
-    bool should_grant_vote(SocketAddress address)
-    {
-        for (auto &[id, node] : nodes)
-        {
-            if (!node.is_alive()) continue;
-            if (strcmp(node.get_address().to_string().c_str(), address.to_string().c_str()) < 0)
-            {
-                log_debug("Not granting vote to ", address.to_string(), "; ", node.get_address().to_string()," has a higher priority.");
-                return false;
-            }
-        }
-
-        return true;
-    }
+    void check_if_won_election();
+    unsigned int get_quota();
 
 public:
-    RaftManager(std::map<std::string, Connection>& connections, NodeMap& nodes, Node& local_node, Pipeline& pipeline) : connections(connections), nodes(nodes), local_node(local_node), pipeline(pipeline), timer_id(0), election_time_dis(3000, 4000), leader(nullptr), data_filename(format("%s.raft", local_node.get_id().c_str())), state(FOLLOWER) {
-        // TODO: o election_timeout_dis tem que ser definido com base no `alive`
-        
-        read_data();
-
-        obs_packet_received.on(std::bind(&RaftManager::packet_received, this, _1));        
-        pipeline.attach(obs_packet_received);
-
-        set_election_timer();
-    }
+    RaftManager(std::map<std::string, Connection> &connections, NodeMap &nodes, Node &local_node, Pipeline &pipeline);
 };
