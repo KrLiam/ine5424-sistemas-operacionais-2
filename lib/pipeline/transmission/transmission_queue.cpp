6f4a7e0e4ce79f43617da8cd1554152def004df5
Fabricio Duarte <fabricio.duarte.jr@gmail.com>
2024-10-27 17:39:25 -0300

    não dar timeout no ab confirmation

--- lib/pipeline/transmission/transmission_queue.cpp
+++ lib/pipeline/transmission/transmission_queue.cpp
@@ -283,5 +283,6 @@ void TransmissionQueue::discard_node(const Node& node) {
 
 bool TransmissionQueue::packet_can_timeout(const Packet& packet)
 {
-    return !message_type::is_urb(packet.data.header.get_message_type());
+    const MessageType& type = packet.data.header.get_message_type();
+    return !message_type::is_urb(type) && type != MessageType::AB_CONFIRMATION;
 }
