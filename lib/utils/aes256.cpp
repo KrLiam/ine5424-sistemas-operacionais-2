a118a8ce50857a6973c0648b235667b9d5c8bcce
Fabricio Duarte <fabricio.duarte.jr@gmail.com>
2024-12-01 21:20:55 -0300

    tira o nome do pobre coitado q fez a implementacao de aes256 do codigo

--- lib/utils/aes256.cpp
+++ lib/utils/aes256.cpp
@@ -1,26 +1,3 @@
-/*
- * aes256.cpp
- *
- * Copyright (c) 2014, Danilo Treffiletti <urban82@gmail.com>
- * All rights reserved.
- *
- *     This file is part of Aes256.
- *
- *     Aes256 is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU Lesser General Public License as
- *     published by the Free Software Foundation, either version 2.1
- *     of the License, or (at your option) any later version.
- *
- *     Aes256 is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *     GNU Lesser General Public License for more details.
- *
- *     You should have received a copy of the GNU Lesser General Public
- *     License along with Aes256.
- *     If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include "aes256.h"
 #include "utils/format.h"
 
